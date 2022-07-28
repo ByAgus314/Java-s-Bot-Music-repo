80e008199477841be4991daa842a632f1731df2a
ByAgus314 <agustincollaogonzalez@gmail.com>
2022-07-28 15:23:23 -0400

    a

--- botconfig.js
+++ botconfig.js
@@ -24,9 +24,9 @@ module.exports = {
   // If you get invalid oauth, make sure on the discord developer page you set the oauth url to something like: https://example.com/api/callback.
 
   Presence: {
-    status: "online", // You can show online, idle, and dnd
-    name: "Music", // The message shown
-    type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+    status: "dnd", // You can show online, idle, and dnd
+    name: "InfernumCraft 2", // The message shown
+    type: "STREAMING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
   // You need a lavalink server for this bot to work!!!!
